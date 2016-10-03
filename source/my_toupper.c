426e6c499aff18c0829abd21e72c2944d00a9022
David Bradford <david.bradford@yodle.com>
2016-10-02 20:58:51 -0500

    Add tolower and toupper functions

---
+++ source/my_toupper.c
@@ -0,0 +1,18 @@
+/*
+ * my_upper.c
+ *
+ * Licensed under GNU GPL v3
+ * Contributors:
+ * - dbradf
+ *
+ */
+
+#include <unixlib.h>
+
+char my_toupper(const char c) {
+    if ((c >= 97) && (c <= 122)) {
+        return c - 32;
+    }
+
+    return FAILURE;
+}
