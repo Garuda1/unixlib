f4a572bdf83d5a01b70a3aaf7b7f02f68b4ae499
Thomas Murgia <garuda1@protonmail.com>
2016-10-04 18:53:33 +0100

    Create my_fdgetc.c

---
+++ source/my_fdgetc.c
@@ -0,0 +1,25 @@
+/*
+ * my_fdgetc.c
+ *
+ * Licensed under GNU GPL v3
+ * Contributors:
+ * - Garuda1 <garuda1@protonmail.com>
+ *
+ */
+
+#include <unixlib.h>
+#include <unistd.h>
+
+/*
+ * Returns sizeof(char) bytes read 
+ * from a provided file descriptor,
+ * or FAILURE on error.
+ *
+ */
+
+char my_fdgetc(const int fd)
+{
+  char c;
+
+  return ((read(fd, &c, sizeof(char)) == -1) ? FAILURE : c);
+}
