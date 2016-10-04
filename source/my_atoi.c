31279e29a56a792272958375190c8fddb5b5f753
Thomas Murgia <garuda1@protonmail.com>
2016-10-04 19:14:02 +0100

    Update my_atoi.c

--- source/my_atoi.c
+++ source/my_atoi.c
@@ -4,6 +4,7 @@
  * Licensed under GNU GPL v3
  * Contributors:
  * - varshit97
+ * - Garuda1 <garuda1@protonmail.com>
  *
  */
 
@@ -13,7 +14,7 @@
 int my_atoi(const char *str)
 {
     int value = 0, i;
-    for(i = 0;str[i] != '\0';i++)
+    for(i = 0;str[i] != CHAR_NULL;i++)
 	{
         value = value*10 + str[i] - '0';
 	}
