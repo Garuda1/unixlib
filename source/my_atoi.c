e6215fb1b20af9569cddb684f7c5d7688ee1327f
SametSisartenep <rodrigosloop@gmail.com>
2016-10-07 14:07:49 +0200

    Update Function: my_atoi;

--- source/my_atoi.c
+++ source/my_atoi.c
@@ -5,18 +5,23 @@
  * Contributors:
  * - varshit97
  * - Garuda1 <garuda1@protonmail.com>
+ * - Rodrigo González López (SametSisartenep)
  *
  */
 
 #include<unixlib.h>
 #include<stdio.h>
-  
+
 int my_atoi(const char *str)
 {
-    int value = 0, i;
-    for(i = 0;str[i] != CHAR_NULL;i++)
-	{
-        value = value*10 + str[i] - '0';
+	int value=0, neg=0;
+	while (my_isspace(*str)) str++;
+	switch (*str) {
+	case '-': neg=1;
+	case '+': str++;
 	}
-    return value;
+	/* Work with n as negative to avoid integer overflow */
+	while (my_isdigit(*str))
+		value = 10*value - (*str++ - '0');
+	return neg ? value : -value;
 }
