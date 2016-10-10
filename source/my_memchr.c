803ab5b375619eb114af472f9bb4b36043b41e15
Thomas Murgia <garuda1@protonmail.com>
2016-10-10 11:03:51 +0200

    Create my_memchr.c

---
+++ source/my_memchr.c
@@ -0,0 +1,32 @@
+/*
+ * my_memchr.c
+ *
+ * Licensed under GNU GPL v3
+ * Contributors:
+ * - Garuda1
+ *
+ */
+
+#include <unixlib.h>
+#include <sys/types.h>
+
+/*
+ * This function returns a pointer to the first
+ * iteration of c in str, or NULL if none was
+ * found.
+ *
+ */
+
+void *my_memchr(const void *str, const unsigned char c, const size_t n)
+{
+  size_t count;
+  
+  count = 0;
+  while (count < n)
+  {
+    if (*((unsigned char *)str) == c)
+      return (str + count);
+    ++count;
+  }
+  return (MY_NULL);
+}
