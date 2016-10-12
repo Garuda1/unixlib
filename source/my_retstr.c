339c9765ad88c666bc6ffd22f58a2dc9cb0c4002
Garuda1 <garuda1@protonmail.com>
2016-10-12 08:53:02 +0000

    ninja commit

---
+++ source/my_retstr.c
@@ -0,0 +1,22 @@
+/*
+ * my_retstr.c
+ *
+ * Licensed under GNU GPL v3
+ * Contributors:
+ * - Garuda1 <garuda1@protonmail.com>
+ *
+ */
+
+#include <unixlib.h>
+
+/*
+ * This function prints str to the
+ * standard output and return ret.
+ *
+ */
+
+int my_retstr(const int ret, const char *str)
+{
+  my_puts(str);
+  return (ret);
+}
