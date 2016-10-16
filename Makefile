8fc0f7280634a77c77d1468d4ffd4beaa0b422c6
mattiadg <mattiadigangi@gmail.com>
2016-10-16 22:17:47 +0200

    Added function my_strcat

--- Makefile
+++ Makefile
@@ -67,7 +67,8 @@ SRCS = $(SRC_DIR)/my_putc.c \
        $(SRC_DIR)/linked-list/my_list_append.c \
 			 $(SRC_DIR)/my_strcmp.c \
 			 $(SRC_DIR)/my_strcpy.c \
-			 $(SRC_DIR)/my_strncpy.c
+			 $(SRC_DIR)/my_strncpy.c \
+			 $(SRC_DIR)/my_strcat.c
 
 NAME = unixlib
 OBJS = $(SRCS:.c=.o)
---
+++ source/my_strcat.c
@@ -0,0 +1,32 @@
+/*
+ * my_strcat.c
+ *
+ * Contributors:
+ * - Mattia Antonino Di Gangi <mattiadigangi@gmail.com>
+ *
+ */
+
+#include <unixlib.h>
+
+/*
+ * Appends the string pointed to by src to the end of the string pointed to by dest.
+ * dest should contain a C string, and should be large enough to contain the concatenated resulting string.
+ * src is the string to be appended. This should not overlap the destination.
+ */
+ 
+ char *my_strcat(char *dest, const char *src)
+ {
+	 char* d = dest;
+	 while (*d != CHAR_NULL)
+	 {
+		 d++;
+	 }
+	 while (*src != CHAR_NULL)
+	 {
+		 *d++ = *src++;
+		 
+	 }
+	 *d++ = CHAR_NULL;
+	 return dest;
+}
+	 
